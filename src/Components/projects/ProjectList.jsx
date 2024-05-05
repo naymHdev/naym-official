d97bf23b1930f0806861887f24e2f9884507cb08
MacBook Pro <naym100m@gmail.com>
2024-05-05 21:41:08 +0600

    [added a pagination] in the project page

--- src/Components/projects/ProjectList.jsx
+++ src/Components/projects/ProjectList.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { motion } from "framer-motion";
 import ProjectLayout from "./ProjectLayout";
+import { Pagination } from "antd";
+import { useState } from "react";
 
 const container = {
   hidden: { opacity: 0 },
@@ -14,6 +16,18 @@ const container = {
 };
 
 const ProjectList = ({ projects }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  // Calculate start and end indexes for pagination
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentProperties = projects?.slice(startIndex, endIndex);
+
   return (
     <motion.div
       variants={container}
@@ -21,9 +35,25 @@ const ProjectList = ({ projects }) => {
       animate="show"
       className="w-full max-w-auto  xl:max-w-4xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center"
     >
-      {projects.map((project, index) => {
+      {currentProperties?.map((project, index) => {
         return <ProjectLayout key={index} {...project} />;
       })}
+      <Pagination className="mt-5"
+        current={currentPage}
+        pageSize={itemsPerPage}
+        total={projects?.length}
+        onChange={handlePageChange}
+        showSizeChanger={false}
+        showQuickJumper={true}
+        itemRender={(item, type, originalElement) => (
+          <li
+            key={item.key}
+            className="custom-bg text-white"
+          >
+            {originalElement}
+          </li>
+        )}
+      />
     </motion.div>
   );
 };
