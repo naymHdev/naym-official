687939fec8713cb516d95bd7eceb4501336bc122
MacBook Pro <naym100m@gmail.com>
2024-05-09 00:04:58 +0600

    update a about us page classes

--- src/Components/about/index.jsx
+++ src/Components/about/index.jsx
@@ -28,61 +28,55 @@ const AboutDetails = () => {
           </p>
         </ItemLayout>
 
-        <ItemLayout
-          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
-        >
+        <ItemLayout className="col-span-full xs:col-span-6 lg:col-span-4 text-accent">
           <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
             05+ <sub className="font-semibold text-base">clients</sub>
           </p>
         </ItemLayout>
 
-        <ItemLayout
-          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
-        >
+        <ItemLayout className="col-span-full xs:col-span-6 lg:col-span-4 text-accent">
           <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
             01+
             <sub className="font-semibold text-base">years of experience</sub>
           </p>
         </ItemLayout>
 
-        <ItemLayout
-          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
-        >
+        <ItemLayout className="col-span-full sm:col-span-6 md:col-span-4 !p-0">
           <img
             className="w-full h-auto"
-            src={`https://github-readme-stats.vercel.app/api/top-langs/?username=naymHdev&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            src="https://github-readme-stats.vercel.app/api/top-langs/?username=naymHdev&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false"
             alt="Naym Hossen"
             loading="lazy"
           />
         </ItemLayout>
 
-        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+        <ItemLayout className="col-span-full md:col-span-8 !p-0">
           <img
             className="w-full h-auto"
-            src={`https://github-readme-stats.vercel.app/api?username=naymHdev&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            src="https://github-readme-stats.vercel.app/api?username=naymHdev&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false"
             alt="Naym Hossen"
             loading="lazy"
           />
         </ItemLayout>
-        <ItemLayout className={"col-span-full"}>
+        <ItemLayout className="col-span-full">
           <img
             className="w-full h-auto"
-            src={`https://skillicons.dev/icons?i=html,css,tailwind,bootstrap,figma,firebase,git,github,js,ts,jquery,mongodb,netlify,nodejs,express,react,redux,nextjs,threejs,vercel,vite,vscode,npm,yarn`}
-            alt="Naym Hossen"
-            loading="lazy"
-          />
-        </ItemLayout>
-
-        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
-          <img
-            className="w-full h-auto"
-            src={`https://github-readme-streak-stats.herokuapp.com/?user=naymHdev&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            src="https://skillicons.dev/icons?i=html,css,tailwind,bootstrap,figma,firebase,git,github,js,ts,jquery,mongodb,netlify,nodejs,express,react,redux,nextjs,threejs,vercel,vite,vscode,npm,yarn"
             alt="Naym Hossen"
             loading="lazy"
           />
         </ItemLayout>
 
-        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+        <ItemLayout className="col-span-full md:col-span-6 !p-0">
+          <img
+            className="w-full h-auto"
+            src="https://github-readme-streak-stats.herokuapp.com/?user=naymHdev&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B"
+            alt="Naym Hossen"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className="col-span-full md:col-span-6 !p-0">
           <Link
             href="https://github.com/naymHdev"
             target="_blank"
@@ -90,7 +84,7 @@ const AboutDetails = () => {
           >
             <img
               className="w-full h-auto"
-              src={`https://github-readme-stats.vercel.app/api/pin/?username=naymHdev&repo=Fintex-Fitness-Client-Code&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+              src="https://github-readme-stats.vercel.app/api/pin/?username=naymHdev&repo=Fintex-Fitness-Client-Code&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2"
               alt="Naym Hossen"
               loading="lazy"
             />
--- src/app/(pages)/projects/page.js
+++ src/app/(pages)/projects/page.js
@@ -1,14 +1,13 @@
 import Image from "next/image";
-// import dynamic from "next/dynamic";
 import bg from "../../../../public/background/projects-background.png";
 import ProjectList from "@/Components/projects/ProjectList";
 import RenderModel from "@/Components/RenderModel";
 import { projectsData } from "../../data";
-import Design from "@/Components/models/Design";
+import dynamic from "next/dynamic";
 
-// const Staff = dynamic(() => import("@/components/models/Staff"), {
-//   ssr: false,
-// });
+const Designs = dynamic(() => import("@/Components/models/Design"), {
+  ssr: false,
+});
 
 export const metadata = {
   title: "Projects",
@@ -29,7 +28,7 @@ export default function Projects() {
 
       <div className="flex items-center justify-center fixed  top-16  lg:top-20 -translate-x-1/2 lg:translate-x-0 -z-10 left-1/2 lg:-left-24 h-screen">
         <RenderModel>
-          <Design />
+          <Designs />
         </RenderModel>
       </div>
     </>
